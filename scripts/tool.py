a38048970d1b7c96a48d34bf146e40af8cb03eae
duckly <duckly_lee@163.com>
2018-12-02 13:11:49 +0800

    add tool.py for epmc

--- scripts/tool.py
+++ scripts/tool.py
@@ -3,40 +3,52 @@ from benchmark import Benchmark
 from invocation import Invocation
 from execution import Execution
 
+
 def get_name():
     """ should return the name of the tool as listed on http://qcomp.org/competition/2019/"""
-    return "Storm"
+    return "ePMC"
 
 def is_benchmark_supported(benchmark : Benchmark):
     """ Auxiliary function that returns True if the provided benchmark is supported by the tool"""
 
-    if benchmark.is_pta() and benchmark.is_prism():
-        # The PTAs from Prism are not supported because either
-        # modest can't apply digital clocks semantic due to open constraints, or
-        # modest puts branch-rewards on the models (these are not supported by Storm)
+    if benchmark.is_pta():
+        # PTAs are not supported by ePMC
         return False
-    if benchmark.is_prism_inf() and benchmark.is_ctmc():
-        # Storm does not support the CTMCs with infinite state-spaces
+    if benchmark.is_prism_ma():
+        # MAs are not supported by ePMC
         return False
+    if benchmark.get_short_property_type() == "S":
+		# Steady state properties are not supported by ePMC
+        return False
+    if benchmark.is_prism_inf():
+		# CTMCs with infinite state-spaces are not supported by ePMC
+        return False
+    
     return True
 
 def is_on_benchmark_list(benchmark : Benchmark):
     """ Returns true, if the given benchmark should appear on the generated benchmark list."""
 
-    # do not include for models that storm does not support
+    # do not include for models that ePMC does not support
     if not is_benchmark_supported(benchmark):
         return False
 
     # do not include models with small state spaces, except it's the haddad-monmege one (because we like that)
-    if benchmark.get_max_num_states() is not None and benchmark.get_max_num_states() < 10000 and benchmark.get_model_short_name() != "haddad-monmege":
-        return False
-
-    # do not select dfts with a file parameter "R" that is set to true
+    # if benchmark.get_max_num_states() is not None and benchmark.get_max_num_states() < 10000 and benchmark.get_model_short_name() != "haddad-monmege":
+    #    return False
+    # list of input models ePMC does not support
     if benchmark.is_galileo():
-        for p in benchmark.get_file_parameters():
-            if p["name"] == "R" and p["value"] == True:
-                return False
-
+        return False
+    if benchmark.is_greatspn():
+        return False
+    if benchmark.is_modest():
+        return False
+    if benchmark.is_ppddl():
+        return False
+    if benchmark.is_pgcl():
+        return False
+    if benchmark.is_modest():
+        return False
     return True
 
 def get_invocations(benchmark : Benchmark):
@@ -54,41 +66,22 @@ def get_invocations(benchmark : Benchmark):
     if not is_benchmark_supported(benchmark):
         return []
 
-    # Gather options that are needed for this particular benchmark for any invocation of Storm
+    # Gather options that are needed for this particular benchmark for any invocation of ePMC
     preprocessing_steps = []
     preprocessing_notes = []
     benchmark_settings = ""
-    if benchmark.is_prism() or benchmark.is_prism_ma() and not benchmark.is_pta():
-        benchmark_settings = "--prism {} --prop {} {}".format(benchmark.get_prism_program_filename(), benchmark.get_prism_property_filename(), benchmark.get_property_name())
+    if benchmark.is_prism():
+		# set parameters
+        benchmark_settings = "--model-input-files {} --property-input-files {} --property-input-names {} --translate-messages false".format(benchmark.get_prism_program_filename(), benchmark.get_prism_property_filename(), benchmark.get_property_name())
         if benchmark.get_open_parameter_def_string() != "":
-            benchmark_settings += " --constants {}".format(benchmark.get_open_parameter_def_string())
-        if benchmark.is_ctmc():
-            benchmark_settings += " --prismcompat"
+            benchmark_settings += " --const {}".format(benchmark.get_open_parameter_def_string())
+        # if benchmark.is_ctmc():
+        #    benchmark_settings += " --prismcompat"
     else:
-        # For jani input, it might be the case that preprocessing is necessary using moconv
-        moconv_options = []
-        features = benchmark.get_jani_features()
-        for f in ["arrays", "derived-operators", "functions", "state-exit-rewards"]:
-            if f in features: features.remove(f)
-        if "nondet-selection" in features:
-            moconv_options.append("--remove-disc-nondet")
-            preprocessing_notes.append("Use moconv to handle currently unsupported jani feature 'nondet-selection'.")
-            features.remove("nondet-selection")
-        if len(features) != 0:
-            print("Unsupported jani feature(s): {}".format(features))
-        if benchmark.is_pta():
-            moconv_options.append("--digital-clocks")
-            preprocessing_notes.append("Use moconv to convert the PTA to an MDP using digital-clocks semantics.")
-
-        if len(moconv_options) != 0:
-            preprocessing_steps.append("mono /modest/moconv.exe {} {} --output {} --overwrite".format(benchmark.get_janifilename(), " ".join(moconv_options), "converted_" + benchmark.get_janifilename()))
-            if benchmark.get_open_parameter_def_string() != "":
-                preprocessing_steps[-1] += " --experiment {}".format(benchmark.get_open_parameter_def_string())
-            benchmark_settings = "--jani {} --janiproperty {}".format("converted_" + benchmark.get_janifilename(), benchmark.get_property_name())
-        else:
-            benchmark_settings = "--jani {} --janiproperty {}".format(benchmark.get_janifilename(), benchmark.get_property_name())
-            if benchmark.get_open_parameter_def_string() != "":
-                benchmark_settings += " --constants {}".format(benchmark.get_open_parameter_def_string())
+        # put properties in saparate files 
+        benchmark_settings = "--model-input-files {} --model-input-type jani --property-input-names {} --translate-messages false".format(benchmark.get_janifilename(), benchmark.get_property_name())
+        if benchmark.get_open_parameter_def_string() != "":
+            benchmark_settings += " --const {}".format(benchmark.get_open_parameter_def_string())
 
     invocations = []
 
@@ -101,18 +94,18 @@ def get_invocations(benchmark : Benchmark):
         for prep in preprocessing_steps:
             default_inv.add_command(prep)
         default_inv.note += " " + " ".join(preprocessing_notes)
-    default_inv.add_command("./storm {}".format(benchmark_settings))
+    default_inv.add_command("java -jar ./epmc-standard.jar check {}".format(benchmark_settings))
     invocations.append(default_inv)
 
     # specific settings
+    # dd engine 
+    if benchmark.benchmark.get_short_property_type() == "E" or benchmark.benchmark.get_short_property_type() == "Ei" or benchmark.benchmark.get_short_property_type() == "Eb":
+		return invocations
     specific_inv = Invocation()
     specific_inv.identifier = "specific"
-    specific_inv.note = "Settings specific for this benchmark. Explain here."
-    if len(preprocessing_steps) != 0:
-        for prep in preprocessing_steps:
-            specific_inv.add_command(prep)
-        specific_inv.note += " " + " ".join(preprocessing_notes)
-    specific_inv.add_command("~/storm/build/bin/storm {} --engine hybrid".format(benchmark_settings))
+    specific_inv.note = "Settings specific for this benchmark. Use symbolic model checking algorithms with BDDs"
+    specific_inv.note += " " + " ".join(preprocessing_notes)
+    specific_inv.add_command("java -jar ./epmc-standard.jar check {} --engine dd".format(benchmark_settings))
     invocations.append(specific_inv)
 
     return invocations
@@ -128,12 +121,9 @@ def get_result(benchmark : Benchmark, execution : Execution):
     """
     invocation = execution.invocation
     log = execution.concatenate_logs()
-    pos = log.find("Model checking property \"{}\":".format(benchmark.get_property_name()))
+    pos = log.find("{}:".format(benchmark.get_property_name()))
     if pos < 0:
         return None
-    pos = log.find("Result (for initial states): ", pos)
-    if pos < 0:
-        return None
-    pos = pos + len("Result (for initial states): ")
+    pos = pos + len(benchmark.get_property_name() + ":")
     eol_pos = log.find("\n", pos)
     return log[pos:eol_pos]
