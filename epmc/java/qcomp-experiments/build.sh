9367e1e8dda7ff601aeeb5248a8b43c86dd2d954
Andrea Turrini <andrea.turrini@gmail.com>
2018-12-06 14:54:26 +0800

    Fixed references to javax.json library

--- epmc/java/qcomp-experiments/build.sh
+++ epmc/java/qcomp-experiments/build.sh
@@ -3,7 +3,7 @@ mvn package
 if [ $? -eq 0 ]; then
 	mkdir q;
 	cd q;
-	jar xf ~/.m2/repository/org/glassfish/javax.json/1.0/javax.json-1.0.jar; 
+	jar xf ~/.m2/repository/org/glassfish/javax.json/1.0.4/javax.json-1.0.4.jar; 
 	jar xf ../target/experiments-*.jar
 	jar cfm ../qcomp.jar META-INF/MANIFEST.MF .
 	cd ..
